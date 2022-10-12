c82977c1ecf40f5545768147a4793e9dc77ab249
gghogare <115607434+gghogare@users.noreply.github.com>
2022-10-12 14:06:06 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,5 @@ streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Idli')
-stremlit.text('test')
+streamlit.text('Tea')
                
